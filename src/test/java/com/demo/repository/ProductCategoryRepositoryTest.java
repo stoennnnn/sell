6cdf892083fc7ffb98bd2851a7b0d33615896b8c
stoennnnn <2672575920@qq.com>
2018-11-15 16:59:35 +0800

    junit测试

--- src/test/java/com/demo/repository/ProductCategoryRepositoryTest.java
+++ src/test/java/com/demo/repository/ProductCategoryRepositoryTest.java
@@ -1,10 +1,15 @@
 package com.demo.repository;
 
+import com.demo.SellApplication ;
 import com.demo.dataobject.ProductCategory;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
@@ -13,18 +18,28 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ProductCategoryRepositoryTest {
+    //@Qualifier("dao")
     @Autowired
-    private  ProductCategoryRepository repository;
+    private ProductCategoryRepository repository;
 
     @Test
     public  void findOneTest(){
-        ProductCategory category = repository.findById(1).get();
+        ProductCategory category = repository.findOne(1);
         System.out.print(category.toString());
-        System.out.println(1111);
-        System.out.println(222);
-        //第三次新增
-        System.out.println(333);
-        //第四次新
-        System.out.println(444);
+
+    }
+    @Test
+    public  void saveTest(){
+        ProductCategory productCategory = new ProductCategory();
+        productCategory.setCategoryName("淘宝爆款");
+        productCategory.setCategoryType("1");
+        repository.save(productCategory);
+    }
+    @Test
+    public  void updateTest(){
+        ProductCategory productCategory = new ProductCategory();
+        productCategory.setCategoryName("淘宝爆款");
+        productCategory.setCategoryType("1");
+        repository.save(productCategory);
     }
 }
