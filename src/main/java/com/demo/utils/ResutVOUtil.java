ddbf775fb0ca136472bfce7909a45628d9567ebd
stoennnnn <2672575920@qq.com>
2018-11-23 09:35:18 +0800

    工具类

---
+++ src/main/java/com/demo/utils/ResutVOUtil.java
@@ -0,0 +1,34 @@
+package com.demo.utils;
+
+import com.demo.VO.ResultVO;
+
+/**
+ * Created by 26725 on 2018/11/21.
+ */
+public class ResutVOUtil {
+    /**
+     * 查询成功
+     * @param data
+     * @return
+     */
+    public  static ResultVO success(Object data){
+        ResultVO<Object> resultVO = new ResultVO<>();
+        resultVO.setCode(200);
+        resultVO.setMsg("成功");
+        resultVO.setData(data);
+        return  resultVO;
+    }
+    public  static ResultVO success(){
+        return  success();
+    }
+    /**
+     * 查询失败
+     * @return
+     */
+    public  static  ResultVO failed(Integer code,String msg){
+        ResultVO<Object> resultVO = new ResultVO<>();
+        resultVO.setCode(code);
+        resultVO.setMsg(msg);
+        return  resultVO;
+    }
+}
